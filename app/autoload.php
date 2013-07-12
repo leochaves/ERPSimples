a583e1e9aebc2e75b43378d99c825fa5749272a5
Leonardo Chaves <leochaves@gmail.com>
2013-07-11 22:22:20 -0300

    colocando o framework junto

---
+++ app/autoload.php
@@ -0,0 +1,14 @@
+<?php
+ini_set('xdebug.max_nesting_level',200);
+use Doctrine\Common\Annotations\AnnotationRegistry;
+
+$loader = require __DIR__.'/../vendor/autoload.php';
+
+// intl
+if (!function_exists('intl_get_error_code')) {
+    require_once __DIR__.'/../vendor/symfony/symfony/src/Symfony/Component/Locale/Resources/stubs/functions.php';
+}
+
+AnnotationRegistry::registerLoader(array($loader, 'loadClass'));
+
+return $loader;
